7bd9bd02c5560f17af9660f9a98bd7ece28f28f7
Ahmed Bougacha <ahmed@bougacha.org>
2021-09-27 08:00:00 -0700

    [clang] Sign the destructor pointer passed to __cxa_throw.

---
+++ clang/test/CodeGenCXX/ptrauth-throw.cpp
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 -fptrauth-function-pointer-type-discrimination -triple arm64-apple-ios -fptrauth-calls -fcxx-exceptions -emit-llvm -no-enable-noundef-analysis %s -o - | FileCheck %s
+
+class Foo {
+ public:
+  ~Foo() {
+  }
+};
+
+void f() {
+  throw Foo();
+}
+
+// __cxa_throw is defined to take its destructor as "void (*)(void *)" in the ABI.
+void __cxa_throw(void *exception, void *, void (*dtor)(void *)) {
+  dtor(exception);
+}
+
+// CHECK: define void @_Z1fv()
+// CHECK:  call void @__cxa_throw(ptr %{{.*}}, ptr @_ZTI3Foo, ptr ptrauth (ptr @_ZN3FooD1Ev, i32 0, i64 [[DISC:10942]]))
+
+// CHECK: call void {{%.*}}(ptr {{%.*}}) [ "ptrauth"(i32 0, i64 [[DISC]]) ]
