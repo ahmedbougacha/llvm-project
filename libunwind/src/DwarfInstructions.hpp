fa19c92658d8cba0ec95052b0b3492a56f204658
Ahmed Bougacha <ahmed@bougacha.org>
2021-09-27 08:00:00 -0700

    libunwind/libcxxabi/compiler-rt: arm64e exception handling support.

--- libunwind/src/DwarfInstructions.hpp
+++ libunwind/src/DwarfInstructions.hpp
@@ -22,6 +22,9 @@
 #include "dwarf2.h"
 #include "libunwind_ext.h"
 
+#if __has_feature(ptrauth_calls)
+#include <ptrauth.h>
+#endif
 
 namespace libunwind {
 
@@ -35,6 +38,7 @@ public:
   typedef typename A::sint_t sint_t;
 
   static int stepWithDwarf(A &addressSpace, pint_t pc, pint_t fdeStart,
+                           unw_word_t procInfoFlags,
                            R &registers, bool &isSignalFrame, bool stage2);
 
 private:
@@ -208,7 +212,9 @@ bool DwarfInstructions<A, R>::isReturnAddressSignedWithPC(A &addressSpace,
 
 template <typename A, typename R>
 int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
-                                           pint_t fdeStart, R &registers,
+                                           pint_t fdeStart,
+                                           unw_word_t procInfoFlags,
+                                           R &registers,
                                            bool &isSignalFrame, bool stage2) {
   FDE_Info fdeInfo;
   CIE_Info cieInfo;
@@ -264,7 +270,7 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
       // by a CFI directive later on.
       newRegisters.setSP(cfa);
 
-      pint_t returnAddress = 0;
+      typename R::reg_t returnAddress = 0;
       constexpr int lastReg = R::lastDwarfRegNum();
       static_assert(static_cast<int>(CFI_Parser<A>::kMaxRegisterNumber) >=
                         lastReg,
@@ -397,6 +403,8 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
           newRegisters.setRegister(UNW_PPC64_R2, r2);
       }
 #endif
+
+      newRegisters.normalizeNewLinkRegister(returnAddress, procInfoFlags);
 
       // Return address is address after call site instruction, so setting IP to
       // that does simulates a return.
