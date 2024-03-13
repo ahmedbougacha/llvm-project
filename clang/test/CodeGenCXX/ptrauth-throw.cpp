c8aa7f29cea742ca87843c9a8f6ac0be1be2a644
Ahmed Bougacha <ahmed@bougacha.org>
2024-03-13 16:59:59 -0700

    fixup: ConstantPtrAuth: [clang] Sign the destructor pointer passed to __cxa_throw.

--- clang/test/CodeGenCXX/ptrauth-throw.cpp
+++ clang/test/CodeGenCXX/ptrauth-throw.cpp
@@ -15,9 +15,7 @@ void __cxa_throw(void *exception, void *, void (*dtor)(void *)) {
   dtor(exception);
 }
 
-// CHECK: @_ZN3FooD1Ev.ptrauth = private constant { ptr, i32, i64, i64 } { ptr @_ZN3FooD1Ev, i32 0, i64 0, i64 [[DISC:[0-9]+]] }, section "llvm.ptrauth", align 8
-
 // CHECK: define void @_Z1fv()
-// CHECK:  call void @__cxa_throw(ptr %{{.*}}, ptr @_ZTI3Foo, ptr @_ZN3FooD1Ev.ptrauth)
+// CHECK:  call void @__cxa_throw(ptr %{{.*}}, ptr @_ZTI3Foo, ptr ptrauth (ptr @_ZN3FooD1Ev, i32 0, ptr null, i64 [[DISC:10942]]))
 
 // CHECK: call void {{%.*}}(ptr {{%.*}}) [ "ptrauth"(i32 0, i64 [[DISC]]) ]
