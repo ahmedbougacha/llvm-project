a1473d3990c0479827e82ffe60e14c23561babee
Ahmed Bougacha <ahmed@bougacha.org>
2024-04-17 13:21:06 -0700

    [AArch64][PAC] Add Hardened-pseudo expansion pass.

--- llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
@@ -254,6 +254,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeAArch64Target() {
   initializeAArch64StackTaggingPreRAPass(*PR);
   initializeAArch64LowerHomogeneousPrologEpilogPass(*PR);
   initializeAArch64DAGToDAGISelPass(*PR);
+  initializeAArch64ExpandHardenedPseudosPass(*PR);
   initializeAArch64GlobalsTaggingPass(*PR);
 }
 
@@ -851,6 +852,10 @@ void AArch64PassConfig::addPreEmitPass() {
     // Identify valid eh continuation targets for Windows EHCont Guard.
     addPass(createEHContGuardCatchretPass());
   }
+
+  // Expand hardened pseudo-instructions.
+  // Do this now to enable LOH emission.
+  addPass(createAArch64ExpandHardenedPseudosPass());
 
   if (TM->getOptLevel() != CodeGenOptLevel::None && EnableCollectLOH &&
       TM->getTargetTriple().isOSBinFormatMachO())
