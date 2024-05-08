6b32382451bf095d5fa816acb69949a9d8d54cc4
Ahmed Bougacha <ahmed@bougacha.org>
2024-05-08 10:25:46 -0700

    Address review feedback.
    - LangRef.rst: missing ':', stale example
    - LLParser.cpp: auto *
    - Bitcode: remove ptrty/addrdiscty
    - AsmWriter.cpp: ListSeparator
    - Constants.h: cast<>
    - Constants.cpp: stale NDEBUG
    - Constants.cpp: stale comments
    - Constants.cpp: use getTypeSizeInBits for offset accumulation,
      to allow pointer/integer types without fiddling.
    - ConstantFold.cpp: remove FIXME, should be addressed separately
    - ValueTracking.cpp: remove from isGuaranteedNotToBeUndefOrPoison

--- llvm/lib/AsmParser/LLParser.cpp
+++ llvm/lib/AsmParser/LLParser.cpp
@@ -4071,7 +4071,7 @@ bool LLParser::parseValID(ValID &ID, PerFunctionState *PFS, Type *ExpectedTy) {
     if (!Ptr->getType()->isPointerTy())
       return error(ID.Loc, "signed pointer must be a pointer");
 
-    auto KeyC = dyn_cast<ConstantInt>(Key);
+    auto *KeyC = dyn_cast<ConstantInt>(Key);
     if (!KeyC || KeyC->getBitWidth() != 32)
       return error(ID.Loc, "signed pointer key must be i32 constant integer");
 
@@ -4079,7 +4079,7 @@ bool LLParser::parseValID(ValID &ID, PerFunctionState *PFS, Type *ExpectedTy) {
       return error(ID.Loc,
                    "signed pointer address discriminator must be a pointer");
 
-    auto DiscC = dyn_cast<ConstantInt>(Disc);
+    auto *DiscC = dyn_cast<ConstantInt>(Disc);
     if (!DiscC || DiscC->getBitWidth() != 64)
       return error(ID.Loc,
                    "signed pointer discriminator must be i64 constant integer");
